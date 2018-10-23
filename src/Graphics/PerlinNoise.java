7ce49f7736ebb0f04ba66ce4a8220163e2af348e
mitchell-johnstone <mitch2579@gmail.com>
2018-10-22 20:15:16 -0500

    new attempt at Perlin & new worm program Worm follows the mouse, draws history too. perlin still work in progress.

--- src/Graphics/PerlinNoise.java
+++ src/Graphics/PerlinNoise.java
@@ -4,10 +4,68 @@ import java.util.Random;
 
 public class PerlinNoise {
 
+    // Function to linearly interpolate between a0 and a1
+// Weight w should be in the range [0.0, 1.0]
+    private double lerp(double a0, double a1, double w) {
+        return (1.0 - w)*a0 + w*a1;
 
-    /*public PerlinNoise() {
+        // as an alternative, this slightly faster equivalent formula can be used:
+        // return a0 + w*(a1 - a0);
+    }
+
+    // Computes the dot product of the distance and gradient vectors.
+    private double dotGridGradient(int ix, int iy, double x, double y) {
+
+        // Precomputed (or otherwise) gradient vectors at each grid node
+
+        // Compute the distance vector
+        double dx = x - (double)ix;
+        double dy = y - (double)iy;
+
+        // Compute the dot-product
+        return (dx*Gradient[iy][ix][0] + dy*Gradient[iy][ix][1]);
+    }
+
+    // Compute Perlin noise at coordinates x, y
+    public double perlin(double x, double y) {
+
+        // Determine grid cell coordinates
+        int x0 = (int)(x);
+        int x1 = x0 + 1;
+        int y0 = (int)(y);
+        int y1 = y0 + 1;
+
+        // Determine interpolation weights
+        // Could also use higher order polynomial/s-curve here
+        double sx = x - (double)x0;
+        double sy = y - (double)y0;
+
+        // Interpolate between grid point gradients
+        double n0, n1, ix0, ix1, value;
+        n0 = dotGridGradient(x0, y0, x, y);
+        n1 = dotGridGradient(x1, y0, x, y);
+        ix0 = lerp(n0, n1, sx);
+        n0 = dotGridGradient(x0, y1, x, y);
+        n1 = dotGridGradient(x1, y1, x, y);
+        ix1 = lerp(n0, n1, sx);
+        value = lerp(ix0, ix1, sy);
+
+        return value;
+    }
+    private double Gradient[][][];
+    public PerlinNoise(int n) {
+        Gradient = new double[n][n][2];
         Random r = new Random();
-        for (int i = 0; i < p.length; i++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Gradient[i][j][0]=r.nextDouble()*2-1;
+                double t1 = Gradient[i][j][0];
+                double t2 = 1-t1*t1;
+                Gradient[i][j][0]=r.nextInt(2)==1?t2:-1*t2;
+
+            }
+        }
+        /*for (int i = 0; i < p.length; i++) {
             int index = r.nextInt(p.length);
             int tmp = p[index];
             p[index] = p[i];
@@ -16,9 +74,9 @@ public class PerlinNoise {
         for (int i = 0; i < 256; i++) {
             p[i] = permutation[i];
             p[256 + i] = p[i];
-        }
+        }*/
     }
-
+    /*
     static public double noise(double x, double y) {
         int X = (int) Math.floor(x) & 255,                  // FIND UNIT CUBE THAT
                 Y = (int) Math.floor(y) & 255;                  // CONTAINS POINT.
@@ -57,7 +115,7 @@ public class PerlinNoise {
         double u = h < 8 ? x : y,                 // INTO 12 GRADIENT DIRECTIONS.
                 v = h < 4 ? y : h == 12 || h == 14 ? x : z;
         return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
-    }*/
+    }
 
     static final int p[] = new int[512], permutation[] = {151, 160, 137, 91, 90, 15,
             131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23,
@@ -72,5 +130,5 @@ public class PerlinNoise {
             251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
             49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254,
             138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180
-    };
+    };*/
 }
