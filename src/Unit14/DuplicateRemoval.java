40d5e40112a3aef2ac1f89a61c433bff5bdd41c3
JOHNSMIT000 <mitch2579@gmail.com>
2019-02-13 12:16:52 -0600

    test commit on mac computer.

--- src/Unit14/DuplicateRemoval.java
+++ src/Unit14/DuplicateRemoval.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class DuplicateRemoval {
     public static void main(String[] args) {
 //        Random r = new Random();
-        int[] numbers = {2,2,7,1,9,5,5,10,1,6};
+        int[] numbers = {2,2,7,1,9,5,5,10,1,6,2,6,3,3,1,9,6};
         for (int i: numbers) System.out.print(i + ", ");
         System.out.println();
         int[] duplicateFreeByFrequency = frequencyDuplicates(numbers);
@@ -19,7 +19,7 @@ public class DuplicateRemoval {
 
     public static int[] frequencyDuplicates(int[] a){
         ArrayList<Integer> values = new ArrayList<>();
-        ArrayList<Integer> frequency = new ArrayList<>();
+//        ArrayList<Integer> frequency = new ArrayList<>();
         for(int i: a){
             int index = -1;
             for(int j = 0; j<values.size() && index == -1; j++){
@@ -27,17 +27,21 @@ public class DuplicateRemoval {
             }
             if(index == -1){
                 values.add(i);
-                frequency.add(1);
+//                frequency.add(1);
             }
-            else frequency.set(index,frequency.get(index)+1);
+//            else frequency.set(index,frequency.get(index)+1);
         }
-        ArrayList<Integer> nonDuplicates = new ArrayList <>();
-        for(int i = 0; i<values.size();i++){
-            if(frequency.get(i)<2)nonDuplicates.add(values.get(i));
-        }
-        a = new int[nonDuplicates.size()];
-        for(int i = 0; i<a.length; i++){
-            a[i] = nonDuplicates.get(i);
+//        ArrayList<Integer> nonDuplicates = new ArrayList <>();
+//        for(int i = 0; i<values.size();i++){
+//            if(frequency.get(i)<2)nonDuplicates.add(values.get(i));
+//        }
+//        a = new int[nonDuplicates.size()];
+//        for(int i = 0; i<a.length; i++){
+//            a[i] = nonDuplicates.get(i);
+//        }
+        a = new int[values.size()];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = values.get(i);
         }
         return a;
     }
@@ -45,17 +49,17 @@ public class DuplicateRemoval {
     public static int[] sortedDuplicates(int[] a){
         Arrays.sort(a);
         int numberOfSolos =a.length;
-        boolean wasDuplicate = false;
+//        boolean wasDuplicate = false;
         for(int i = 1; i<a.length; i++){
             if(a[i]==a[i-1]){
                 a[i-1]=-1;
-                wasDuplicate = true;
+//                wasDuplicate = true;
                 numberOfSolos--;
-            } else if(wasDuplicate){
+            } /*else if(wasDuplicate){
                 a[i-1] = -1;
                 wasDuplicate = false;
                 numberOfSolos--;
-            }
+            }*/
         }
         int[] nonDuplicates = new int[numberOfSolos];
         int index = 0;
